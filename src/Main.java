8d03b6d960ddcd8544952e5883f149836ab02816
Lakshan Rukantha <64830641+LakshanRukantha@users.noreply.github.com>
2023-04-08 22:15:04 +0530

    Prints name and age

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
 public class Main {
 
     public static void main(String[] args) {
+        //String data type
+        String name = "Lakshan Rukantha";
 
-        System.out.println("Hello Lakshan!");
+        //Int data type
+        int age = 22;
+
+        //Print output
+        System.out.println("Hello " + name);
+        System.out.println("You are " + age + " years old.");
 
     }
 }
